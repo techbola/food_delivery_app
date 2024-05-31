17e5d409bb0abd764e788b653975c42d2490c66a
Adebola <adebola@poptin.com>
2024-05-31 12:33:22 +0100

    header component design

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque totam
+          debitis nulla accusamus quasi. Magni, aspernatur. Necessitatibus
+          nulla, rerum dolores est quod iste magni. In veniam delectus animi vel
+          obcaecati? Velit optio porro repellendus, neque rerum beatae in quos,
+          autem temporibus aliquam sit incidunt? Nostrum sequi fugiat commodi
+          dolorem ipsa vel nihil tempore incidunt, delectus labore, nemo maxime?
+          Voluptatem, quisquam? Perferendis totam enim, deleniti illo libero
+          dolores ea tempore quasi mollitia atque hic, cupiditate aut, tenetur
+          sapiente animi alias magni ab illum vitae iure? Voluptas laudantium
+          rem excepturi fugiat voluptatibus.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
